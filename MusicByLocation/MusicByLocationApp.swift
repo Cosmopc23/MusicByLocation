be4842bcb431dee1b4d875ccdeb25228422dd5c6
Cosmo Croft <cosmopagecroft@etoncollege.org.uk>
2024-03-20 21:25:00 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Cosmo Page-Croft on 20/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
